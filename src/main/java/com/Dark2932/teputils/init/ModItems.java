c8b158eaecb84de4d8c82095966c6b46ee8512bb
Dark2932 <72404859+Dark2932@users.noreply.github.com>
2022-03-15 23:45:52 +0800

    add item

--- src/main/java/com/Dark2932/teputils/init/ModItems.java
+++ src/main/java/com/Dark2932/teputils/init/ModItems.java
@@ -11,5 +11,6 @@ public class ModItems {
     public static final List<Item> ITEMS = new ArrayList<Item>();
     public static final Item HEAD_DARK = new ItemBase("head_dark");
     public static final Item HEAD_FURONG = new ItemBase("head_furong");
+    public static final Item HEAD_YW = new ItemBase("head_yw");
 
 }
